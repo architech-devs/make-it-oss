b1a1cfdae22a888e8d4e9a6461b520e90ebd0372
architech-devs <architech.devs@gmail.com>
2025-10-05 19:39:33 +0530

    fixed footer

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { ThemeProvider } from "@/components/Theme/theme-provider"
-import Landing from "./pages/Landing"
+import Landing from "@/pages/Landing"
+import Docs from "@/pages/Docs"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/docs" element={<Docs />} />
         </Routes>
       </ThemeProvider>
     </BrowserRouter>
--- app/client/src/components/Footer/Footer.tsx
+++ app/client/src/components/Footer/Footer.tsx
@@ -1,12 +1,23 @@
 import Discord_logo from "@/assets/images/discord.png"
+import Github_logo from "@/assets/images/github.png"
 import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
-    <div className="flex pb-2 items-center justify-center gap-2">
-      Docs |
-      <img src={Discord_logo} alt="" className="w-4 h-4" /> |
-      <Link to="https://github.com/architech-devs"> The Architech Team</Link>
+    <div className="flex flex-col pb-2 items-center justify-center gap-2">
+      <div className="flex items-center justify-center gap-2">
+        <Link to="/docs" className="hover:text-muted-foreground transition-colors duration-200">Docs</Link>
+        |
+        <Link to="https://discord.gg/r9jzAFU3FM"><img src={Discord_logo} alt="" className="w-4 h-4" /></Link>
+        |
+        <img src={Github_logo} alt="" className="w-4 h-4 dark:invert" />
+      </div>
+      <Link
+        to="https://github.com/architech-devs"
+        className="hover:text-muted-foreground transition-colors duration-200"
+      >
+        The Architech Team
+      </Link>
     </div>
   )
 }
